ead5889fb847f9edf2bc0d2b24a2e68e18e282b6
clarkbyu <clarkbrown@byu.net>
2020-03-21 11:04:34 -0600

    Initial commit

---
+++ webcam_face_detection.py
@@ -0,0 +1,9 @@
+# webcam_face_detection.py
+"""
+Webcam Face Detection
+
+@author: Clark Brown
+@date: 21 March 2020
+
+Based on the tutorial code at https://github.com/DilanKalpa/FaceDetection
+"""
